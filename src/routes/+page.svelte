f74cedd5723530c026f53be909d7369d7515856d
dalgardnerd <41878198+dalgardnerd@users.noreply.github.com>
2023-07-04 12:12:26 -0600

    feature complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
 
-  function createPassphrase () {
-    return "passphrase"
-  }
+  import { onMount } from "svelte";
+  import { createPassphrase } from "$lib/utils/createPassphrase";
 
-  
-
-	let passphrase: string = createPassphrase();
+  let passphrase: string = "";
   let count = 0;
+  let words: string[] = [];
+
+  onMount(async () => {
+		const response = await fetch('basic.txt');
+		const text = await response.text();
+		words = text.split('\n');
+		passphrase = createPassphrase(5,12,3,2,'-',words)
+	});
 
   function handleGet() {
-    count++;
-    passphrase = "eyyo" + count;
+    passphrase = createPassphrase(5,12,3,2,'-',words);
   }
 
-	export async function copyClipboard(text: string): Promise<void> {
+
+	async function copyClipboard(text: string): Promise<void> {
 		try {
 			await navigator.clipboard.writeText(text);
 			console.log('Text copied to clipboard:', text);
@@ -25,22 +30,17 @@
 
 </script>
 
-
-
-
-
-
-  <div class="card w-96 bg-neutral text-primary-content m-2 border-white border-4">
+  <div class="card w-96 bg-neutral text-primary-content border-2 border-white rounded-none">
     <div class="card-body">
-      <h2 class="text-xl text-center bold p-0 m-0">tiny passhole</h2>
-      <p class="text-center break-words m-4">{passphrase}</p>
+      <h2 class="text-2xl text-center bold p-0 m-0">tiny passhole</h2>
+      <p class="flex justify-center text-center align-center text-md bg-red m-2">{passphrase}</p>
       <div class="card-actions justify-center">
         <button
-				class="btn-sm rounded btn-primary lowercase"
+				class="btn-sm rounded btn-secondary lowercase"
 				on:click={handleGet}>Create Passphrase</button
 			>
 				<button
-					class="btn-sm rounded btn-secondary lowercase"
+					class="btn-sm rounded btn-accent lowercase"
 					on:click={() => copyClipboard(passphrase)}>Copy Passphrase</button
 				>
 
